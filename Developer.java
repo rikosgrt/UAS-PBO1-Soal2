8b82eb00700d30c865a54883d3397b3d1f9b9fff
rikosgrt <122290423+rikosgrt@users.noreply.github.com>
2024-01-02 22:40:25 +0700

    Create Developer.java

---
+++ Developer.java
@@ -0,0 +1,14 @@
+package soal2;
+
+public class Developer extends Employee {
+    // Constructor
+    public Developer(String name) {
+        super(name);
+    }
+
+    // Implementasi metode work() untuk Developer
+    @Override
+    public void work() {
+        System.out.println("Developer " + getName() + " is coding and developing system ");
+    }
+}
